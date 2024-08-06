12617c021c388229dfb3635391608fd7d71d3432
MVRA <61065@icf.com>
2024-08-06 17:23:54 -0500

    Add lsp config with tsserver

--- lua/mvra/init.lua
+++ lua/mvra/init.lua
@@ -5,6 +5,7 @@ require('gitsigns').setup{
     current_line_blame=true
 }
 require'lspconfig'.pyright.setup{}
+require'lspconfig'.tsserver.setup{}
 
 vim.keymap.set('n', '<leader>p', require('telescope.builtin').find_files, { desc = 'Search files (like CTRL+P)' })
 vim.keymap.set('n', '<leader>f', require('telescope.builtin').live_grep , { desc = 'Live grep (like CTRL+SHIFT+F)' })
